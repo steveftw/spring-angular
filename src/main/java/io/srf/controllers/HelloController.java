919ae7f688fba1c6f754723be59d1d554f55e725
Steve Fitz <steve.ftw.2017@gmail.com>
2018-01-07 16:24:01 +0000

    initial commit

---
+++ src/main/java/io/srf/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package io.srf.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "{\"message\": \"Hello, World!\"}";
+    }
+
+}
---
+++ src/main/ui/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  message: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('/hello').subscribe(data => {
+      console.log('DATA', data);
+      this.message = data['message'];
+    })
+  }
+}
